8dc82f9337fa0e230e5f9c56eb89385634ecfce0
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-01-09 20:04:09 -0800

    still working on button animation

--- src/Todo.js
+++ src/Todo.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addTodo, removeOne, clearTodo } from './features/todoSlice'
 
+
 function Todo() {
     const items = useSelector((state) => state.todos.items)
     const dispatch = useDispatch()
@@ -17,13 +18,13 @@ function Todo() {
     return (
         <div>
             <form onSubmit={(e) => submitForm(e)}>
-                <input type="text" onChange={(e) => setInput(e.target.value)} />
-                <button type="submit">Submit</button>
+                <input class="button" type="text" onChange={(e) => setInput(e.target.value)} />
+                <button className="button1" type="submit">Submit</button>
             </form>
             <ul>
                 {renderItems}
             </ul>
-            <button onClick={() => dispatch(clearTodo())}>Clear</button>
+            <button className="button2" onClick={() => dispatch(clearTodo())}>Clear</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { store } from './store'
 import { Provider } from 'react-redux'
 
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
